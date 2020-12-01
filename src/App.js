00a4565084063bc880a6223fa53b0f391ef32821
derekcodingtemple <derekh@codingtemple.com>
2020-12-01 15:13:48 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/';
 import { Switch, Route } from 'react-router-dom';
-import Home from './components/views/Home';
-import About from './components/views/About';
-import Contact from './components/views/Contact';
+import Home from './views/Home';
+import About from './views/About';
+import Contact from './views/Contact';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Derek Hawkins",
+      racers: [],
+      stuff: '',
+    }
+  }
+
+  getStuff = () => {
+    this.setState({
+        stuff: 'Something Else'
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    var year = e.target.year.value;
+    var season = e.target.season.value;
+
+    fetch(`https://ergast.com/api/f1/${year}/${season}/driverStandings.json`)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                racers: data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+            })
+        })
+        .catch(error => console.error(error))
+
+    e.target.reset();
+  }
+  
   render() {
     return (
       <div>
@@ -13,9 +47,9 @@ export default class App extends Component {
 
         <main className="container">
           <Switch>
-            <Route exact path='/' render={() => <Home />} />
-            <Route path='/about' render={() => <About />} />
-            <Route path='/contact' render={() => < Contact/>} />
+            <Route exact path='/' render={() => <Home handleSubmit={this.handleSubmit} name={this.state.name} racers={this.state.racers} />} />
+            <Route path='/about' render={() => <About name={this.state.name} getStuff={this.getStuff} />} />
+            <Route path='/contact' render={() => < Contact name={this.state.name} stuff={this.state.stuff} />} />
           </Switch>
         </main>
 
