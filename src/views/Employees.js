eefa620c13f28a280db3138bfd95fd180741dcc6
derekcodingtemple <derekh@codingtemple.com>
2020-12-07 15:23:55 -0600

    update

--- src/views/Employees.js
+++ src/views/Employees.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import firebase from '../firebase'
+import firebase from '../firebase';
 
 export default class Employees extends Component {
     constructor() {
         super();
 
         this.state = {
-            employees: []
+            employees: [],
+            employee: {}
         }
 
-        this.db = firebase.database()
     }
 
     componentDidMount() {
-        this.db.ref().once('value').then(snapshot => this.setState({ employees: snapshot.val() }))
+        const db = firebase.database().ref()
+        db.on('value', snapshot => {
+            const data = [];
+
+            snapshot.forEach(e => {
+                data.push(e.val())
+            })
+            this.setState({ employees: data })
+        })
     }
 
     addToDatabase = () => {
@@ -33,16 +41,44 @@ export default class Employees extends Component {
                 "Flask": 10
             }
         });
-        alert("It worked!");
+    }
+
+    viewEmployeeData = (employee) => {
+        this.setState({ employee })
     }
 
     render() {
+        const employees = this.state.employees;
+        const employee = this.state.employee;
+
         return (
-            <div>
-                Employees view.
-                <div>
-                    <button onClick={this.addToDatabase} className="btn btn-danger">Write some data</button>
+            <div className="row">
+
+                <div className="col-6">
+                    <ul className="list-group">
+                        <li className="list-group-item active">Employees</li>
+                        {employees ? employees.map(e => <li onClick={() => this.viewEmployeeData(e)} key={e.id} className="list-group-item">{`${e.firstName} ${e.lastName}`}</li>) : <h4>Loading...</h4>}
+                    </ul>
                 </div>
+
+                {
+                    employee ? (
+                        <div className="col-6">
+                            <ul className="list-group">
+                                <li className="list-group-item active">{employee.firstName} {employee.lastName}</li>
+                                <li className="list-group-item">City <span className="float-right">{employee.city}</span></li>
+                                <li className="list-group-item">State <span className="float-right">{employee.state}</span></li>
+                                <li className="list-group-item">
+                                    Skills
+                                    {Object.keys(employee).length ? Object.entries(employee.skills).map(s => (
+                                        <div key={s.id}>{s[0]}  <span className="float-right">{s[1]}</span></div>
+                                    )) : null}
+                                </li>
+                            </ul>
+                        </div>
+                    ) : null
+                }
+                
             </div>
         )
     }
